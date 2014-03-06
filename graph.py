5b4d8c1841d5f2817ae39f61b7cc970783f622d3
M42 <mromang08@gmail.com>
2014-03-06 11:28:53 +0100

    Grafos en Python. Usando la implementación de los vértices.

--- graph.py
+++ graph.py
@@ -4,6 +4,7 @@
 
 class Vertex:
     def __init__(self,key):
+        """ El vértice  """
         self.id = key
         self.connectedTo = {}
 
@@ -25,3 +26,44 @@ class Vertex:
     def getWeight(self,nbr):
         """ Devuelve peso de la arista. """
         return self.connectedTo[nbr]
+
+
+class Graph:
+    def __init__(self):
+        """ Construye un grafo vacío. """
+        self.vertList = {}
+        self.numVertices = 0
+
+    def addVertex(self,key):
+        """ Añade un vértice al grafo, dado su valor. """
+        self.numVertices = self.numVertices + 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+
+    def getVertex(self,n):
+        """ Devuelve el vértice de clave dada. """
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+
+    def __contains__(self,n):
+        """ Devuelve si n está en la lista de vértices. """ 
+        return n in self.vertList
+
+    def addEdge(self,f,t,cost=0):
+        """ Añade un vértice a la lista de vértices. """
+        if f not in self.vertList:
+            nv = self.addVertex(f)
+        if t not in self.vertList:
+            nv = self.addVertex(t)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
+
+    def getVertices(self):
+        """ Devuelve la lista de vértices. """
+        return self.vertList.keys()
+
+    def __iter__(self):
+        """ El iterador sobre el grafo es el iterador sobre la lista de vértices. """
+        return iter(self.vertList.values())
